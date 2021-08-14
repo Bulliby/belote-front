14af49c22f58b3001772b6d55efdd56566e519c7
Guillaume Wells <wellsguillaume@gmail.com>
2021-08-14 19:01:52 +0200

    Chain provide with createApp and mount function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './components/App.vue'
 
-createApp(App).mount('#app')
 
 const ws = new WebSocket('ws://127.0.0.1:6001');
+
+createApp(App).provide('ws', ws).mount('#app');
 
 var hand = null;
 
